ddb4aa361d80e42c42ad1d4527e72da190dca150
Jihee Choi <jchoi0428@gmail.com>
2022-01-18 16:30:12 -0500

    set up home page

---
+++ netflix_clone/src/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.scss"
+import {AcUnit} from "@material-ui/icons"
+
+const Home = () => {
+  return (
+    <div className="home">
+        <AcUnit/>
+    </div>
+  );
+};
+
+export default Home;
